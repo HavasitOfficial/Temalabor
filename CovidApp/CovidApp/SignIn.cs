466de57aafa9344f83c46404456002e913e93006
HavasitOfficial <aszodimiki@gmail.com>
2021-03-25 21:21:00 +0100

    A személynek a felhasználónevének és jelszavának az ellenőrzésének a függvény megírva: SignIn class

---
+++ CovidApp/CovidApp/SignIn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidApp
+{
+    class SignIn
+    {
+        private Boolean registerAllow = false;
+
+        public SignIn()
+        {
+
+        }
+        public void CheckUserAndPass(string username, string password, List<string> user, List<string> pass)
+        {
+            var resultUser = user.Select((Value, Index) => new { Value, Index })
+                        .SingleOrDefault(l => l.Value == username);
+
+            var indexUser = resultUser == null ? -1 : resultUser.Index;
+
+            var resultPass = pass.Select((Value, Index) => new { Value, Index })
+                        .SingleOrDefault(l => l.Value == password);
+
+            var indexPass = resultPass == null ? -1 : resultPass.Index;
+
+            if (indexUser == indexPass && resultUser != null && resultPass != null) this.registerAllow = true;
+        }
+    }
+}
