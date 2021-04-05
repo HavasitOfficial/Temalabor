ef487423ace8dc9820f5eb5f6f0c252b902b8ee5
HavasitOfficial <aszodimiki@gmail.com>
2021-04-05 22:22:11 +0200

    Nuget-ek installálva SignIn clickben popUp előugrás kész

--- CovidApp/CovidApp/MainPage.xaml.cs
+++ CovidApp/CovidApp/MainPage.xaml.cs
@@ -23,7 +23,7 @@ namespace CovidApp
 
         private void SignIn_Click(object sender, RoutedEventArgs e)
         {
-
+            popUp.IsOpen = true;
         }
 
         private void Statics_Click(object sender, RoutedEventArgs e)
