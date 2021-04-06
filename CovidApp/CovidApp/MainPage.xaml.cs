7ab4661982b3f13ab02d0d123edd223c9121c6d6
HavasitOfficial <aszodimiki@gmail.com>
2021-04-06 15:05:46 +0200

    Betöltési idő csökkentve A kódolt jelszó fekódolásának a függvénye megírva

--- CovidApp/CovidApp/MainPage.xaml.cs
+++ CovidApp/CovidApp/MainPage.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -14,8 +17,58 @@ namespace CovidApp
         public MainPage()
         {
             this.InitializeComponent();
+            /*var key = "b14ca5898a4e4133bbce2ea2315a1916";
+
+            //Console.WriteLine("Please enter a secret key for the symmetric algorithm.");  
+            //var key = Console.ReadLine();  
+
+            Console.WriteLine("Please enter a string for encryption");
+            //var str = Console.ReadLine();
+            
+            var encryptedString = encrypt(key,"jelszo123");
+            var encryptedString2 = encrypt(key,"jelszo456");
+            var encryptedString3 = encrypt(key,"jelszo789");
+            Console.WriteLine($"encrypted string = {encryptedString}");
+
+            var decryptedString = DecryptString(key, encryptedString);
+            Console.WriteLine($"decrypted string = {decryptedString}");
+
+            new Windows.UI.Popups.MessageDialog($"encrypted string1 = {encryptedString}").ShowAsync();
+            new Windows.UI.Popups.MessageDialog($"encrypted string2 = {encryptedString2}").ShowAsync();
+            new Windows.UI.Popups.MessageDialog($"encrypted string3 = {encryptedString3}").ShowAsync();
+            new Windows.UI.Popups.MessageDialog($"encrypted string3 = {decryptedString}").ShowAsync();
+                */
+
         }
 
+        private String encrypt(string key, string plainText )
+        {
+            
+            byte[] iv = new byte[16];
+            byte[] array;
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+            return Convert.ToBase64String(array);
+        }
+        
         private void Enter_Click(object sender, RoutedEventArgs e)
         {
 
--- CovidApp/CovidApp/SignIn.cs
+++ CovidApp/CovidApp/SignIn.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,25 +10,61 @@ namespace CovidApp
 {
     class SignIn
     {
+        private string key = "b14ca5898a4e4133bbce2ea2315a1916";
         private Boolean registerAllow = false;
-
+        private List<string> descryptedPass;
         public SignIn()
         {
 
         }
         public void CheckUserAndPass(string username, string password, List<string> user, List<string> pass)
         {
+            descryptPassword(pass);
+
             var resultUser = user.Select((Value, Index) => new { Value, Index })
                         .SingleOrDefault(l => l.Value == username);
 
             var indexUser = resultUser == null ? -1 : resultUser.Index;
 
-            var resultPass = pass.Select((Value, Index) => new { Value, Index })
+            var resultPass = descryptedPass.Select((Value, Index) => new { Value, Index })
                         .SingleOrDefault(l => l.Value == password);
 
             var indexPass = resultPass == null ? -1 : resultPass.Index;
 
             if (indexUser == indexPass && resultUser != null && resultPass != null) this.registerAllow = true;
         }
+
+
+        private string DecryptString(string key, string cipherText)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+        private void descryptPassword(List<string> sv)
+        {
+            foreach (var item in sv)
+            {
+                this.descryptedPass.Add(DecryptString(this.key,item));
+            }
+        }
+
     }
 }
